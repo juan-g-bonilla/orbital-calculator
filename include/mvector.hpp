6a49b9934f95a658bd6e0d7881867ddf546062b2
lJuanGB <lJuanGBMinecraft@gmail.com>
2020-05-27 04:29:31 +0200

    Declaration and implementation of MVector

---
+++ include/mvector.hpp
@@ -0,0 +1,59 @@
+#ifndef MATH_MVECTOR_HPP
+#define MATH_MVECTOR_HPP
+
+#include <initializer_list>
+#include <stdexcept>
+#include <ostream>
+
+/**
+ *  Represents a vector in  the mathematical sense of the word
+ *  which implements useful operators for linear algebra operations
+ */
+class MVector
+{
+    public:
+    MVector(unsigned int size);
+    MVector(std::initializer_list<double> list);
+    ~MVector();
+    MVector(const MVector& source);
+    MVector& operator=(const MVector& source);
+    MVector(MVector&& source);
+    MVector& operator=(MVector&& source);
+
+    /// @return same vector
+    MVector operator+();
+    /// @return sum of two vectors
+    MVector operator+(MVector& other);
+    /// @return vector with same magnitudes but opposite sign
+    MVector operator-();
+    /// @return vector which is this - other elements
+    MVector operator-(MVector& other);
+    /// @return Dot product of two vectors
+    MVector operator*(MVector& other);
+    /** @return Cross product of two vectors
+     *  @throw std::invalid_argument if either this or other is not size 3
+     */
+    MVector operator^(MVector& other);
+
+    /** @return access to number at index @param n
+     *  @throw std::out_of_bounds
+     */
+    double& operator[](const unsigned int n);
+
+    /// @return norm of vector
+    double norm();
+
+    /// @return size of the vector
+    unsigned int size();
+
+    private:
+    unsigned int _size;
+    double* data;
+
+    /// Used internally to initialize with a size with 0 or not
+    MVector(unsigned int size, bool initializeZeros);
+};
+
+std::ostream &operator<<(std::ostream &os, MVector &v);
+
+#endif
---
+++ src/mvector.cpp
@@ -0,0 +1,175 @@
+#include "mvector.hpp"
+#include <cmath>
+
+using std::pow;
+
+MVector::MVector(unsigned int __size, bool _initialize)
+{
+    if (__size == 0)
+        throw std::invalid_argument("Size must be greater than 0");
+
+    _size = __size;
+
+    if (_initialize)
+        data = new double[_size] {};
+    else
+        data = new double[_size];
+}
+
+MVector::MVector(unsigned int _size) : MVector(_size, true) {};
+
+MVector::MVector(std::initializer_list<double> list)
+{
+    _size = list.size();
+
+    if (_size == 0)
+        throw std::invalid_argument("Size must be greater than 0");
+
+    data = new double[_size];
+
+    for (int i = 0; i < _size; i++)
+        data[i] = *(list.begin() + i);
+}
+
+MVector::~MVector()
+{
+    delete[] data;
+}
+
+MVector::MVector(const MVector& source)
+{
+    _size = source._size;
+    data = new double[_size];
+
+    for (int i = 0; i < _size; i++)
+        data[i] = source.data[i];
+}
+
+MVector& MVector::operator=(const MVector& source)
+{
+    if (this == &source)
+            return *this;
+    delete[] data;
+
+    _size = source._size;
+    data = new double[_size];
+
+    for (int i = 0; i < _size; i++)
+        data[i] = source.data[i];
+
+    return *this;
+}
+
+MVector::MVector(MVector&& source)
+{
+    _size = source._size;
+    data = source.data;
+    source.data = nullptr;
+}
+
+MVector& MVector::operator=(MVector&& source)
+{
+    if (this == &source)
+            return *this;
+    delete[] data;
+
+    _size = source._size;
+    data = source.data;
+    source.data = nullptr;
+    return *this;
+}
+
+MVector MVector::operator+()
+{
+    return *this;
+}
+
+MVector MVector::operator+(MVector& other)
+{
+    if (_size != other._size)
+        throw std::invalid_argument("Vector size must coincide");
+
+    MVector result(_size, false);
+    for (int i = 0; i < _size; i++)
+        result[i] = (*this)[i] + other[i];
+    
+    return std::move(result);
+}
+
+MVector MVector::operator-()
+{
+    MVector result(_size, false);
+    for (int i = 0; i < _size; i++)
+        result[i] = -(*this)[i];
+    
+    return std::move(result);
+}
+
+MVector MVector::operator-(MVector& other)
+{
+    if (_size != other._size)
+        throw std::invalid_argument("Vector size must coincide");
+
+    MVector result(_size, false);
+    for (int i = 0; i < _size; i++)
+        result[i] = (*this)[i] - other[i];
+    
+    return std::move(result);
+}
+
+MVector MVector::operator*(MVector& other)
+{
+    if (_size != other._size)
+        throw std::invalid_argument("Vector size must coincide");
+
+    MVector result(_size, false);
+    for (int i = 0; i < _size; i++)
+        result[i] = (*this)[i] * other[i];
+    
+    return std::move(result);
+}
+
+MVector MVector::operator^(MVector& other)
+{
+    if (_size != other._size || _size != 3)
+        throw std::invalid_argument("Vectors size must be 3");
+
+    MVector result(3, false);
+    result[0] = (*this)[1]*other[2] - (*this)[2]*other[1];
+    result[1] = (*this)[2]*other[0] - (*this)[0]*other[2];
+    result[2] = (*this)[0]*other[1] - (*this)[1]*other[0];
+
+    return std::move(result);
+}
+
+double& MVector::operator[](const unsigned int n)
+{
+    return data[n];
+}
+
+double MVector::norm()
+{
+    double result;
+    for (int i = 0; i < _size; i++)
+    {
+        result += pow(data[i],2);
+    }
+    return pow(result, 0.5);
+}
+
+unsigned int MVector::size()
+{
+    return _size;
+}
+
+std::ostream &operator<<(std::ostream &os, MVector &v)
+{ 
+    for (int i = 0; i < v.size(); i++)
+    {
+        os << v[i];
+        if (i != v.size() -1)
+            os << ",";
+    }
+
+    return os;
+}
