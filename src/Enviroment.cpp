4ca9f32189b5018dd90536932530da1c17286908
Juan Garcia Bonilla <juan@garciabonilla.com>
2020-06-02 04:07:20 +0200

    Implement up to J3 oblateness effects

--- src/Enviroment.cpp
+++ src/Enviroment.cpp
@@ -64,11 +64,31 @@ void Enviroment::setPropagator(std::unique_ptr<Propagator>&& propagator)
     this->propagator = std::move(propagator);
 }
 
-// Current implementation is two-body
+// Current implementation can handle up to J3 oblateness
 MVector Enviroment::getAcceleration(EphemerisEntry entry)
 {
-    MVector r = {entry.getX(), entry.getY(), entry.getZ()};
-    return -centralBody.getGravitationalParameter()/std::pow(r.norm(),3)*r;
+    MVector rv = {entry.getX(), entry.getY(), entry.getZ()};
+    double r = rv.norm();
+    MVector rdd = -centralBody.getGravitationalParameter()/std::pow(r,3)*rv;
+
+    if (centralBody.isJefferyConstantSet(2))
+    {
+        double J2 = centralBody.getJefferyConstant(2);
+        rdd[0] += J2*rdd[0]/std::pow(r,7)*(6*std::pow(rv[2],2) - 1.5*(std::pow(rv[0],2) + std::pow(rv[1],2)));
+        rdd[1] += J2*rdd[1]/std::pow(r,7)*(6*std::pow(rv[2],2) - 1.5*(std::pow(rv[0],2) + std::pow(rv[1],2)));
+        rdd[2] += J2*rdd[2]/std::pow(r,7)*(3*std::pow(rv[2],2) - 4.5*(std::pow(rv[0],2) + std::pow(rv[1],2)));
+    }
+
+    if (centralBody.isJefferyConstantSet(3))
+    {
+        double J3 = centralBody.getJefferyConstant(3);
+        rdd[0] += J3*rdd[0]*rdd[2]/std::pow(r,9)*(10*std::pow(rv[2],2) - 7.5*(std::pow(rv[0],2) + std::pow(rv[1],2)));
+        rdd[1] += J3*rdd[1]*rdd[2]/std::pow(r,9)*(10*std::pow(rv[2],2) - 7.5*(std::pow(rv[0],2) + std::pow(rv[1],2)));
+        rdd[2] += J3/std::pow(r,9)*(4*std::pow(rdd[2],2)*(std::pow(rdd[2],2) - 3*(std::pow(rv[0],2) + std::pow(rv[1],2)) ) 
+                                    + 1.5*std::pow( (std::pow(rv[0],2) + std::pow(rv[1],2)) ,2) );
+    }
+
+    return rdd;
 }
 
 int Enviroment::propagate()
